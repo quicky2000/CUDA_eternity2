596527673201e4dff20fce82c24a43760e636dfe
Julien Thevenon <julien_thevenon@yahoo.fake.fr>
2016-09-08 23:55:38 +0200

    Move code related to eternity2 pieces in main.cpp to reuse it in various kernels

--- src/eternity2.cu
+++ src/eternity2.cu
@@ -120,113 +120,10 @@ CUDA_KERNEL(kernel, const piece * const p_pieces, constraint (*p_constraints)[18
 }
 
 //------------------------------------------------------------------------------
-int launch_cuda_code(void)
+int launch_cuda_code(piece (&p_pieces)[197], constraint (&p_constraints)[18][4])
 {
   test_constraint();
   test_orientation();
-
-  // Binary representation of pieces
-  piece l_pieces[197];
-
-  uint32_t l_center_id_to_piece_id[196];
-
-  // Binary representation of constraints by colors
-  // Color 0 represent no pieces
-  constraint l_constraints[18][4];
-  for(unsigned int l_index = 0; l_index < 4 ; ++l_index)
-    {
-      l_constraints[0][l_index].fill(true);
-    }
-
-  unsigned int l_color_id_to_center_color_id[23] = 
-    {
-      256,
-      256,
-      256,
-      256,
-      256,
-      256,
-      256,
-      256,
-      256,
-      256,
-      256,
-      256,
-      256,
-      256,
-      256,
-      256,
-      256,
-      256,
-      256,
-      256,
-      256,
-      256,
-      256
-    };
-
-  unsigned int l_center_color_id_to_color_id[18] = 
-    {
-      128,
-      128,
-      128,
-      128,
-      128,
-      128,
-      128,
-      128,
-      128,
-      128,
-      128,
-      128,
-      128,
-      128,
-      128,
-      128,
-      128,
-      128
-    };
-
-  // Compute pieces bitfield representation
-  unsigned int l_center_piece_index = 1;
-
-  unsigned int l_center_color_index = 1;
-
-#include "eternity2_pieces.h"
-
-  for(unsigned int l_all_pieces_index = 0; l_all_pieces_index < 256; ++l_all_pieces_index)
-    {
-      if(0 != l_all_pieces[l_all_pieces_index][1 + (unsigned int)t_orientation::NORTH] &&
-	 0 != l_all_pieces[l_all_pieces_index][1 + (unsigned int)t_orientation::EAST] &&
-	 0 != l_all_pieces[l_all_pieces_index][1 + (unsigned int)t_orientation::SOUTH] &&
-	 0 != l_all_pieces[l_all_pieces_index][1 + (unsigned int)t_orientation::WEST]
-	 )
-	{
-	  for(unsigned int l_orientation_index = (unsigned int)t_orientation::NORTH; l_orientation_index <= (unsigned int)t_orientation::WEST; ++l_orientation_index)
-	    {
-	      unsigned int l_color_id = l_all_pieces[l_all_pieces_index][1 + l_orientation_index];
-
-	      // Compute center_color_id if not already done
-	      if(256 == l_color_id_to_center_color_id[l_color_id])
-		{
-		  l_color_id_to_center_color_id[l_color_id] = l_center_color_index;
-		  l_center_color_id_to_color_id[l_center_color_index] = l_color_id;
-		  ++l_center_color_index;
-		}
-	      unsigned int l_center_color_id = l_color_id_to_center_color_id[l_color_id];
-	      // Store bitfield representation
-	      l_pieces[l_center_piece_index].set_color(l_center_color_id, l_orientation_index);
-
-	      // Record this color in constraint table. We consider l_center_piece_index - 1 because piece 0 mean no piece
-	      l_constraints[l_center_color_id][l_orientation_index].set_bit(l_center_piece_index - 1);
-	    }
-
-	  // Keep memory of global piece id
-	  l_center_id_to_piece_id[l_center_piece_index] = l_all_pieces[l_all_pieces_index][0];
-
-	  ++l_center_piece_index;
-	}
-    }
 
   int l_initial_index = 17;
 
@@ -252,8 +149,8 @@ int launch_cuda_code(void)
   gpuErrChk(cudaMalloc(&l_initial_situation_ptr, sizeof(situation)));
   gpuErrChk(cudaMalloc(&l_initial_index_ptr, sizeof(int)));
   
-  gpuErrChk(cudaMemcpy(l_pieces_ptr, &l_pieces[0], 197 * sizeof(piece), cudaMemcpyHostToDevice));
-  gpuErrChk(cudaMemcpy(l_constraints_ptr, &l_constraints[0][0], 18 * 4 * sizeof(constraint), cudaMemcpyHostToDevice));
+  gpuErrChk(cudaMemcpy(l_pieces_ptr, &p_pieces[0], 197 * sizeof(piece), cudaMemcpyHostToDevice));
+  gpuErrChk(cudaMemcpy(l_constraints_ptr, &p_constraints[0][0], 18 * 4 * sizeof(constraint), cudaMemcpyHostToDevice));
   gpuErrChk(cudaMemcpy(l_initial_situation_ptr, &l_initial_situation, sizeof(situation), cudaMemcpyHostToDevice));
   gpuErrChk(cudaMemcpy(l_initial_index_ptr, &l_initial_index, sizeof(int), cudaMemcpyHostToDevice));
   
--- src/main.cpp
+++ src/main.cpp
@@ -16,12 +16,120 @@
 */
 
 #include "border_enumeration.h"
+#include "piece.h"
+#include "constraint.h"
+#include "eternity2_types.h"
 
-int launch_cuda_code(void);
+int launch_cuda_code(piece (&p_pieces)[197], constraint (&p_constraints)[18][4]);
 
 int main(void)
 {
   enumerate();
-  return launch_cuda_code();
+
+  // Binary representation of pieces
+  piece l_pieces[197];
+
+  uint32_t l_center_id_to_piece_id[196];
+
+  // Binary representation of constraints by colors
+  // Color 0 represent no pieces
+  constraint l_constraints[18][4];
+  for(unsigned int l_index = 0; l_index < 4 ; ++l_index)
+    {
+      l_constraints[0][l_index].fill(true);
+    }
+
+  unsigned int l_color_id_to_center_color_id[23] = 
+    {
+      256,
+      256,
+      256,
+      256,
+      256,
+      256,
+      256,
+      256,
+      256,
+      256,
+      256,
+      256,
+      256,
+      256,
+      256,
+      256,
+      256,
+      256,
+      256,
+      256,
+      256,
+      256,
+      256
+    };
+
+  unsigned int l_center_color_id_to_color_id[18] = 
+    {
+      128,
+      128,
+      128,
+      128,
+      128,
+      128,
+      128,
+      128,
+      128,
+      128,
+      128,
+      128,
+      128,
+      128,
+      128,
+      128,
+      128,
+      128
+    };
+
+  // Compute pieces bitfield representation
+  unsigned int l_center_piece_index = 1;
+
+  unsigned int l_center_color_index = 1;
+
+#include "eternity2_pieces.h"
+
+  for(unsigned int l_all_pieces_index = 0; l_all_pieces_index < 256; ++l_all_pieces_index)
+    {
+      if(0 != l_all_pieces[l_all_pieces_index][1 + (unsigned int)t_orientation::NORTH] &&
+	 0 != l_all_pieces[l_all_pieces_index][1 + (unsigned int)t_orientation::EAST] &&
+	 0 != l_all_pieces[l_all_pieces_index][1 + (unsigned int)t_orientation::SOUTH] &&
+	 0 != l_all_pieces[l_all_pieces_index][1 + (unsigned int)t_orientation::WEST]
+	 )
+	{
+	  for(unsigned int l_orientation_index = (unsigned int)t_orientation::NORTH; l_orientation_index <= (unsigned int)t_orientation::WEST; ++l_orientation_index)
+	    {
+	      unsigned int l_color_id = l_all_pieces[l_all_pieces_index][1 + l_orientation_index];
+
+	      // Compute center_color_id if not already done
+	      if(256 == l_color_id_to_center_color_id[l_color_id])
+		{
+		  l_color_id_to_center_color_id[l_color_id] = l_center_color_index;
+		  l_center_color_id_to_color_id[l_center_color_index] = l_color_id;
+		  ++l_center_color_index;
+		}
+	      unsigned int l_center_color_id = l_color_id_to_center_color_id[l_color_id];
+	      // Store bitfield representation
+	      l_pieces[l_center_piece_index].set_color(l_center_color_id, l_orientation_index);
+
+	      // Record this color in constraint table. We consider l_center_piece_index - 1 because piece 0 mean no piece
+	      l_constraints[l_center_color_id][l_orientation_index].set_bit(l_center_piece_index - 1);
+	    }
+
+	  // Keep memory of global piece id
+	  l_center_id_to_piece_id[l_center_piece_index] = l_all_pieces[l_all_pieces_index][0];
+
+	  ++l_center_piece_index;
+	}
+    }
+
+
+  return launch_cuda_code(l_pieces, l_constraints);
 }
 // EOF
